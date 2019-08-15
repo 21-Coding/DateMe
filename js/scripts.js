14cc0b25fd7c78730266baa641ed15393601e189
Student <example@example.com>
2019-08-15 10:53:06 -0700

    create a webpage that allows for the user to input infomation about themselves and then get matched with a celeb

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+  $("form#Dating-Form").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+    var eyeColor = $("select#EyeColor").val();
 
-  if (over21) {
-    $('#drinks').show();
-  } else {
-    $('#under-21').show();
-  }
+    var quote = "is "
+    if (gender === 'male' && age < 26 && eyeColor === 'Brown') {
+      quote += 'Sting';
+    }
+    if (gender === 'female' && age > 26 && eyeColor === 'Green') {
+      quote += 'Brittany Spears';
+    }
+
+    $("#rate").text(quote);
+    $("#quote").show()
+
+    event.preventDefault();
+  });
 });
